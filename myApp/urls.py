ee7101a40fffb1e4a3f62490b052c35688a2380f
Betto Calero <calero.bd@gmail.com>
2024-03-09 22:01:23 -0300

    Segundo commit

--- myApp/urls.py
+++ myApp/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("inicio/", views.inicio),
-    path("nueva_pelicula/", views.agregar_pelicula),
-    path("nueva_serie/", views.agregar_serie),
-    path("nuevo_libro/", views.agregar_libro),
+    path("inicio/", views.inicio, name='inicio'),
+    path("nueva_pelicula/", views.agregar_pelicula, name='pelicula'),
+    path("nueva_serie/", views.agregar_serie, name='serie'),
+    path("nuevo_libro/", views.agregar_libro, name= 'libro'),
+    path("buscar_libro/", views.buscar_libro, name='buscar_libro'),
+    path("resultado/", views.resultado_libro, name='resultado'),
 ]
--- myApp/views.py
+++ myApp/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import *
+from .forms import *
 from django.http import HttpResponse
 
 # Create your views here.
@@ -9,13 +11,82 @@ def inicio(request):
 
 def agregar_pelicula(request):
 
-    return render(request, 'myApp/nueva_pelicula.html')
+    if request.method == 'POST':
+        formulario = PeliculaFormulario(request.POST)
+
+        if formulario.is_valid():
+            info_dict = formulario.cleaned_data
+
+            nueva_pelicula = Pelicula(nombre = info_dict['nombre'],
+                                      descriptcion = info_dict['descriptcion'],
+                                      fecha_estreno = info_dict['fecha_estreno'])
+            
+            nueva_pelicula.save()
+
+            return render(request, 'myApp/inicio.html')
+    
+    else:
+        formulario = PeliculaFormulario()
+
+    return render(request, 'myApp/nueva_pelicula.html', {'form' : formulario})
 
 def agregar_serie(request):
+
+    if request.method == 'POST':
+
+        formulario = SerieFormulario(request.POST)
+
+        if formulario.is_valid():
+            info_dict = formulario.cleaned_data
+
+            nueva_serie = Serie(nombre = info_dict['nombre'],
+                                descripcion = info_dict['descripcion'],
+                                fecha_estreno = info_dict['fecha_estreno'],
+                                temporadas = info_dict['temporadas'],
+                                cant_capitulos = info_dict['cant_capitulos'])
+            
+            nueva_serie.save()
+
+            return render(request, 'myApp/inicio.html')
+        
+    else:
+        formulario = SerieFormulario
     
-    return render(request, 'nueva_serie.html')
+    return render(request, 'myApp/nueva_serie.html', {'form' : formulario})
 
 def agregar_libro(request):
+
+    if request.method == 'POST':
+
+        formulario = LibroFormulario(request.POST)
+
+        if formulario.is_valid():
+            info_dict = formulario.cleaned_data
+
+            nueva_pelicula = Libro(nombre = info_dict['nombre'],
+                                      descripcion = info_dict['descripcion'],
+                                      autor = info_dict['autor'],
+                                      fecha_publicacion = info_dict['fecha_publicacion'])
+            
+            nueva_pelicula.save()
+
+            return render(request, 'myApp/inicio.html')
+        
+    else:
+        formulario = LibroFormulario()
     
-    return render(request, 'nuevo_libro.html')
+    return render(request, 'myApp/nuevo_libro.html', {'form' : formulario})
+
+
+def buscar_libro(request):
+
+    return render(request, 'myApp/buscar_libro.html')
+
+def resultado_libro(request):
+
+    libro = request.GET['libro']
+
+    resultado = Libro.objects.filter(nombre__iexact=libro)
+
+    return render(request, 'myApp/resultado_libro.html', {'resultado' : resultado})
 
